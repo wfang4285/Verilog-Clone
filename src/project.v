b6e906978d1e10e87d505a373d4ed3b759b644a2
wfang4285 <wfang6763@gmail.com>
2024-07-02 17:36:31 -0400

    FSM Test

--- src/project.v
+++ src/project.v
@@ -13,15 +13,55 @@ module wfang4285 (
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire       rst_n,    // reset_n - low to reset
+    input  wire       sensor,   //State representing the sensor
+    input wire        arm,      //State representing armed alarm.
+    output reg        alarm,    //Alarm output.
+    input wire        on        //Alarm on/off
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  //FSM representing security chip based on "Sensors."
+  typedef enum reg [1:0] {
+      off = 2'b00,
+      armed = 2'b01,
+      triggered = 2'b10,
+      alarm = 2'b11
+  } state_t;
+
+  reg [1:0] state;
+  reg [1:0] next_state;
+  
+  //Checking state and assigning your next_state.
+  always @(*) begin
+    case(state)
+      off: if (arm) next_state = armed;
+           else next_state = off;
+      armed: if (sensor) next_state = triggered;
+             else next_state = armed;
+      triggered: if (on) next_state = alarm;
+             else next_state = triggered;
+      alarm: next_state = alarm;
+      default: next_state = off;
+    endcase 
+  end
+
+  //Updates state and status of alarm. 
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      state <= off;
+      alarm <= 0;
+    end else begin
+      state <= next_state;
+      if (state == alarm) begin
+        alarm <= 1;
+      end else begin
+        alarm <= 0;
+      end
+    end
+  end
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+  wire _unused = &{ena, rst_n, 1'b0};
+  assign uio_oe = 8'b0; 
+  assign uio_out = 8'b0; 
 endmodule
--- test/test.py
+++ test/test.py
@@ -28,13 +28,17 @@ async def test_project(dut):
     # Set the input values you want to test
     dut.ui_in.value = 20
     dut.uio_in.value = 30
+    dut.arm = 1
+    dut.sensor = 0
+    dut.on = 0
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 10)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 70
+    assert dut.status == dut.armed
+    assert dut.next_status == dut.triggered
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
